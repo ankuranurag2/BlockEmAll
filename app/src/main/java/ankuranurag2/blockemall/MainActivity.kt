62cf026486c6b3c4ba5c388c26fb9c1a4029464a
ankuranurag2 <ankuranurag2@gmail.com>
2020-12-28 19:00:33 +0530

    DataBinding For MainActivity

--- app/src/main/java/ankuranurag2/blockemall/MainActivity.kt
+++ app/src/main/java/ankuranurag2/blockemall/MainActivity.kt
@@ -2,10 +2,15 @@ package ankuranurag2.blockemall
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import ankuranurag2.blockemall.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
     }
 }
